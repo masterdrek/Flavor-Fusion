35a73d42bd49a0d9b6dbcb47095a71cf2f09a9de
derek <dereklee1124@gmail.com>
2024-06-01 12:48:37 -0700

    allowing unused vars

--- packages/react-frontend/.eslintrc.cjs
+++ packages/react-frontend/.eslintrc.cjs
@@ -15,6 +15,7 @@ module.exports = {
         "no-unused-vars": "off",
         "react/jsx-no-target-blank": "off",
         "react/prop-types": "off",
+        "react-hooks/exhaustive-deps": "off",
         "react-refresh/only-export-components": [
             "warn",
             { allowConstantExport: true }
