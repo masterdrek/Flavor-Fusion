eac41439e989c398f16b6e960e0a81ce175b82f3
hliu34 <hliu.xian025@gmail.com>
2024-06-02 15:22:54 -0700

    removed request functions in the backend server that were never used

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -169,61 +169,6 @@ app.delete("/recipes/:recipeId", async (req, res) => {
     }
 });
 
-// for the ingredient references
-app.get("/ingredient-references", async (req, res) => {
-    const result = await ingredientReferenceServices.getAll();
-    if (!result.length) {
-        return res.status(400).json({ message: "References not found" });
-    }
-    return res.json(result);
-});
-
-app.post("/ingredient-references", async (req, res) => {
-    const { name } = req.body;
-    if (!name) {
-        return res.status(400).json({ message: "Needs name field" });
-    }
-
-    const duplicate = await ingredientReferenceServices.getByName({ name });
-    if (duplicate) {
-        return res.status(400).json({ message: "Duplicate ingredient found" });
-    }
-
-    const result = await ingredientReferenceServices.add({ name });
-    if (!result) {
-        return res.status(400).json({ message: "Could not add ingredient" });
-    }
-    return res.status(201).json({ message: "Ingredient reference added" });
-});
-
-app.patch("/ingredient-references", async (req, res) => {
-    const { id, name } = req.body;
-    if (!id || !name) {
-        return res
-            .status(400)
-            .json({ message: "Needs old reference 'id' and new 'name fields" });
-    }
-
-    const result = await ingredientReferenceServices.update({ id, name });
-    if (!result) {
-        return res.status(400).json({ message: "Could not update ingredient" });
-    }
-    return res.status(200).json({ message: "Ingredient reference updated" });
-});
-
-app.delete("/ingredient-references", async (req, res) => {
-    const { id } = req.body;
-    if (!id) {
-        return res.status(400).json({ message: "Needs id" });
-    }
-
-    const result = await ingredientReferenceServices.remove({ id });
-    if (!result) {
-        return res.status(400).json({ message: "Ingredient not found" });
-    }
-    return res.status(200).json({ message: "Ingredient reference deleted" });
-});
-
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
